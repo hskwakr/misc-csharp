8cb7a5078454f025b5b4f8a8693f1b8a007b113f
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-06-19 06:50:49 +0900

    Move files from misc repo

---
+++ src/advent-of-code/AoC2015/AoC/ApartmentInstruction.cs
@@ -0,0 +1,53 @@
+﻿namespace AoC
+{
+    public class ApartmentInstruction
+    {
+        private string _construction;
+
+        public ApartmentInstruction(string construction)
+        {
+            SetConstruction(construction);
+        }
+
+        public string Construction
+        {
+            get { return _construction; }
+        }
+
+        public int Floor { get; set; }
+
+        public void SetConstruction(string construction)
+        {
+            _construction = construction;
+        }
+
+        public int CulculateFloor()
+        {
+            foreach (var instruction in Construction)
+            {
+                switch (instruction)
+                {
+                    case '(':
+                        GoUpFloor();
+                        break;
+
+                    case ')':
+                        GoDownFloor();
+                        break;
+                }
+            }
+
+            return Floor;
+        }
+
+        private void GoUpFloor()
+        {
+            Floor++;
+        }
+
+        private void GoDownFloor()
+        {
+            Floor--;
+        }
+    }
+}
---
+++ src/advent-of-code/AoC2015/AoC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace AoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1();
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2015/day/1
+        /// </summary>
+        static void Day1()
+        {
+            var input = InputData.Get(1) as string;
+            var apartment = new ApartmentInstruction(input);
+            var result = apartment.CulculateFloor();
+
+            Console.WriteLine(result);
+        }
+    }
+}
